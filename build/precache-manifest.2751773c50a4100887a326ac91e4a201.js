539c91703dd0f72c84447af08fa4ffaa605570ef
prateek-saxena-07 <86514419+prateek-saxena-07@users.noreply.github.com>
2022-12-25 22:55:26 +0530

    Add files via upload

---
+++ build/precache-manifest.2751773c50a4100887a326ac91e4a201.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "efec4546fb8569c6044da881ceac0409",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bc529ac95c533d97745d",
+    "url": "/static/css/main.a25574c1.chunk.css"
+  },
+  {
+    "revision": "6e2d7a9fbc772ec3686f",
+    "url": "/static/js/2.1f771bf1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.1f771bf1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc529ac95c533d97745d",
+    "url": "/static/js/main.01b5260e.chunk.js"
+  },
+  {
+    "revision": "7402b3a51c6a0325ccf5",
+    "url": "/static/js/runtime-main.297f6f71.js"
+  }
+]);
